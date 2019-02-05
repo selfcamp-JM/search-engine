b442354d4e4e57d66939229ffe6ecce5319aa43d
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-02-05 05:09:41 +0300

    The index page is being ready)

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 
 <html>
   <head>
-    <title>
-
-    </title>
+    <title>Kovuyov</title>
   </head>
  <body>
+    <div class ="mainSection">
+   
+      <div class ="logoContainer">
+         <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />
+      </div>
+      <div class="seachContainer">
+
+        <input class="searchBox" type="text" name="term">
+        <input class="searchButton" type="submit" value="Search">
+      
+      </div>
     
+    
+    </div>
  </body>
 </html>
