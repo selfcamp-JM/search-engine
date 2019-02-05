37cfa5fe04e1cfd30957804457c47d49ae5a8603
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-02-05 06:52:03 +0300

    style.css is added

--- index.php
+++ index.php
@@ -7,21 +7,27 @@
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
   </head>
  <body>
-    <div class ="mainSection">
+ 
+ <div class="wrapper indexPage">
+
+ <div class ="mainSection">
    
-      <div class ="logoContainer">
-         <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />
-      </div>
-      <div class="seachContainer">
-      
-        <form action="search.php" method = "GET">
-          <input class="searchBox" type="text" name="term">
-          <input class="searchButton" type="submit" value="Search">  
-        </form>
-      
-      </div>
-    
-    
-    </div>
+   <div class ="logoContainer">
+      <img src="assets/image/festisite_google.png" />
+   </div>
+   <div class="seachContainer">
+   
+     <form action="search.php" method = "GET">
+       <input class="searchBox" type="text" name="term">
+       <input class="searchButton" type="submit" value="Search">  
+     </form>
+   
+   </div>
+ 
+ 
+ </div>
+
+ </div>
+
  </body>
 </html>
