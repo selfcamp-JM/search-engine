1f6245c40a5390b64798c2975a58eef702a51373
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-02-08 01:21:41 +0300

    .

--- crawl.php
+++ crawl.php
@@ -1,6 +1,9 @@
 <?php 
 include('classes/DomDocumnetParser.php');
 
+$alreadyCrawled =array();
+$crawling =array();
+
 function createLink($src, $url){
    
    $scheme = parse_url($url)["scheme"]; //http
@@ -11,11 +14,23 @@ function createLink($src, $url){
    }
    else if(substr($src,0,1)=="/"){
       $src = $scheme."://".$host.$src;
+   }else if(substr($src,0,2)=="./"){
+      $src = $scheme."://".$host.dirname(parse_url($url)["path"]).substr($src, 1);
    }
+   else if(substr($src,0,3)=="../"){
+      $src = $scheme."://".$host."/".$src;
+   }else if(substr($src,0,5)!="https" && substr($src,0,4)!="http"){
+      $src = $scheme."://".$host."/".$src;
+   }
+
    return $src;
 }
 
 function followLinks($url){
+
+  global $alreadyCrawled;
+  global $crawling;
+
   $parser = new DomDocumentParser($url);
   $linkList = $parser->getLinks();
   
@@ -34,9 +49,22 @@ function followLinks($url){
      
     $href = createLink($href,$url);
 
+    if(in_array($href, $alreadyCrawled)){
+    	$alreadyCrawled[] = $href;
+    	$crawling[]=$href;
+    }
+
      echo $href."<br>";
   }  
+  array_shift($crawling);
+
+  foreach ($crawling as $site) {
+  	followLinks($site);
+  }
+
 }
+
+
 
 $startUrl ="http://www.bbc.com";
 followLinks($startUrl);
