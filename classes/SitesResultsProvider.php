856e559888b56aecc467a69bd562ef6303646dcd
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-02-13 02:46:57 +0300

    Url,description

--- classes/SitesResultsProvider.php
+++ classes/SitesResultsProvider.php
@@ -41,8 +41,20 @@ public function getResultsHtml($page, $pageSize, $term) {
 		
 		while($row = $query->fetch(PDO::FETCH_ASSOC)) {
           
+          $id = $row["id"];
+          $url = $row["url"];
           $title = $row["title"];
-          $resultsHtml .= "$title <br>";
+          $description = $row["description"];
+
+          $resultsHtml .="<div class='resultContainer'>
+                            <h3 class='title'>
+                            <a class='result' href='$url'>
+                              $title
+                            </a>
+                            </h3>
+                            <span class='url'>$url</span>
+                            <span class='description'>$description</span>
+                          </div>";
         }
 		
 		$resultsHtml .= "</div>";
