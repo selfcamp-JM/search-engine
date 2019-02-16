853cf10080729946dc0b8f35909ccec28cd5bb13
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-02-16 06:02:53 +0300

    ImageSEARCH working

--- classes/ImageResultProvider.php
+++ classes/ImageResultProvider.php
@@ -29,10 +29,10 @@ public function getResultsHtml($page, $pageSize, $term) {
         $fromLimit = ($page-1)*$pageSize;
 
 	 	$query = $this->con->prepare("SELECT *  
-										 FROM sites WHERE title LIKE :term 
-										 OR url LIKE :term 
-										 OR keywords LIKE :term 
-										 OR description LIKE :term
+										 FROM images 
+										 WHERE (title LIKE :term 
+										 OR alt LIKE :term)
+										 AND broken=0
 										 ORDER BY clicks DESC
 										 LIMIT :fromLimit,:pageSize");
 
@@ -42,26 +42,36 @@ public function getResultsHtml($page, $pageSize, $term) {
 		$query->bindParam(":pageSize", $pageSize, PDO::PARAM_INT);
 		$query->execute();
 
-        $resultsHtml = "<div class = 'siteResults'>";
+        $resultsHtml = "<div class = 'imageResults'>";
 		
 		while($row = $query->fetch(PDO::FETCH_ASSOC)) {
           
           $id = $row["id"];
-          $url = $row["url"];
+          $imageUrl = $row["imageURL"];
           $title = $row["title"];
-          $description = $row["description"];
+          $alt = $row["alt"];
+          $siteURL = $row["siteURL"];
+
+
+          if($title){
+             $displayText  = $title;
+          }elseif ($alt) {
+          	 $displayText = $alt;
+          }else{
+          	$displayText = $imageUrl;
+          }
 
           //$title = $this->trimField($title,55);
           //$description = $this->trimField($description,230);
 
-          $resultsHtml .="<div class='resultContainer'>
-                            <h3 class='title'>
-                            <a class='result' href='$url' data-linkId='$id'>
-                              $title
+          $resultsHtml .="<div class='gridItem'>
+                            
+                            <a href='$imageUrl'>
+                              <img src='$imageUrl'>
                             </a>
                             </h3>
-                            <span class='url'>$url</span>
-                            <span class='description'>$description</span>
+                           
+                            
                           </div>";
         }
 		
